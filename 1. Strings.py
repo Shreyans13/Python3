6f940c41d7a744cf1ff84ac990c0ca59be4318be
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-01-11 15:43:04 +0530

    Create 1. Strings.py

---
+++ 1. Strings.py
@@ -0,0 +1,23 @@
+name="shreyans jain"
+print ("name = ",name)
+print ("name.title() = ",name.title())
+print ("name.upper() = ",name.upper())
+print ("name.lower() = ",name.lower())
+
+first_name="shreyans"
+last_name="jain"
+full_name=f"{first_name} {last_name}" #This strings are called f-strings(format strings)
+print ("full_name = ",full_name)
+
+print (f"Hello, {full_name}!!!")
+
+message=f"Hello, {full_name.title()}!!!"
+print (message)
+
+print()
+print ("Languages:\n\tC\n\tC++\n\tJAVA\n\tPYTHON\n\tHTML5\n\tCSS\n\tJAVASCRIPT\n\tNODE.JS")
+favourite_language="                PyThOn              "
+print ("favourite_language = ",favourite_language)
+print ("favourite_language.rstrip() = ",favourite_language.rstrip())
+print ("favourite_language.lstrip() = ",favourite_language.lstrip())
+print ("favourite_language.strip() = ",favourite_language.strip())
