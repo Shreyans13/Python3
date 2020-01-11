216aebd348a266a79d62836be38a8e73058f1858
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-01-11 15:42:59 +0530

    Create 0. HelloWorld.py

---
+++ 0. HelloWorld.py
@@ -0,0 +1,19 @@
+print("Hello python interpreter")
+
+print()
+print ("import this")
+import this
+
+print()
+a='I told you my friend, "Phython is my favourite language".'
+print(a)
+b="The language 'python' is named after Monty Python, not the snake."
+print(b) 
+c="One of Python's strengths is its diverse and supportive community."
+print(c)
+
+print()
+print("DataTypes")
+print("type(13)",type(13))
+print("type('17')",type('17'))
+print("type(13.17)",type(13.17))
