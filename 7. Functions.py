e77e83e267f7e6e0f6e0ffd8c7d6d84892df1de3
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-01-29 03:08:27 +0530

    Functions 1

--- 7. Functions.py
+++ 7. Functions.py
@@ -17,8 +17,30 @@ describe_pet(animal_type = 'Cat', pet_name = 'kitty') #keyword Arguments
 # or
 describe_pet(pet_name = 'rosie', animal_type = 'Cat')
 
-def message(user = 'user'):
+def message(user = 'user'): #default value
+# Multiple default values can also be set.
     print (f"\tHello, {user.title()}") #Display a greeting a message
 
 message("Shreyans Jain")
 message()
+#optional argument value
+def get_formatted_name(first_name, last_name,  middle_name=''):
+    message = f"\tWe are hackers, and hackers have black terminals with green font.\n"
+    if middle_name:
+        return f"{message.upper()} \t\t{first_name} {middle_name} {last_name}"
+    return f"{message.upper()} \t\t{first_name} {middle_name} {last_name}"
+
+print(get_formatted_name('Shreyans','Jain'))
+print(get_formatted_name('Suraj', 'Kumar', 'Singh'))
+
+
+print (f"\tDictionary Builder")
+def build_dictionary(first_name, last_name, age = None):
+    person = {
+    'name' : f"{first_name} {last_name}"
+    }
+    if age:
+        person['age'] = age
+    return person
+print ("\tDictionary : ",build_dictionary("Shreyans", "Jain",age = 20))
+print ("\tDictionary : ",build_dictionary(input("\tEnter your first name : "), input("\tEnter your last name : ")))
