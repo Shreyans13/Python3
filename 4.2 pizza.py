d4ce54d6f3ebf28ce0ac2e34184d3a7cdc9d541d
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-01-29 03:08:07 +0530

    4.2 debug removed

--- 4.2 pizza.py
+++ 4.2 pizza.py
@@ -1,7 +1,7 @@
 import random
 requested_toppings = ({'pepperoni','cheese burst'},{'mushrooms','extra cheese'},{})[random.randint(0,2)]
-print("\tAutomatic pizza generator")
-print(requested_toppings)
+print("Automatic pizza generator")
+#print(requested_toppings)
 if requested_toppings:
     if 'mushrooms' in requested_toppings:
         print("\tAdding mushrooms.")
@@ -14,4 +14,3 @@ if requested_toppings:
     print("\nFinished making your pizza!")
 else:
     print("\tNo Pizza for you")
-    
