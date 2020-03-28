8512fc7bfeeefac3f8bcdc9596e897d470b2717d
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-03-29 00:02:00 +0530

    Update 8.1 Classes.py

--- 8.1 Classes.py
+++ 8.1 Classes.py
@@ -26,3 +26,48 @@ print(f"\tMy dog is {your_dog.name}.")
 print(f"\tMy dog is {your_dog.age} is years old.")
 your_dog.roll_over()
 your_dog.sit()
+
+
+# Working with classes and instance
+
+class Car(object):
+	"""A simple attempt to represent a car."""
+	def __init__(self, make , model ,year):
+		"""Initialize attributes to describe a car"""
+		# super(Car, self).__init__()
+		self.make = make
+		self.model = model
+		self.year = year
+		self.odometer_reading = 0
+
+	def get_descriptive_name(self):
+		"""Return a neatly formatted descriptive name"""
+		return f"{self.year} {self.make} {self.model}".title()
+	
+	def read_odometer(self):
+		"""Print a statement showing car`s mileage """
+		print(f"This car has {self.odometer_reading} miles on it.")
+
+	def update_odometer(self, mileage):
+		"""
+		Set the odometer reading to the given value.
+		Reject the change if it attempts to roll the odometer back.
+		"""
+		if mileage >= self.odometer_reading:
+			self.odometer_reading = mileage
+		else:
+			print("You cant! roll back an odometer!")
+
+	def incrememt_odometer(self, miles):
+		"""Add the given amount to odometer reading"""
+		self.odometer_reading += miles
+
+my_new_car = Car('audi','a13',2019)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(130)
+my_new_car.read_odometer()
+
+my_new_car.incrememt_odometer(20)
+my_new_car.read_odometer()
